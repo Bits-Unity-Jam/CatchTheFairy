180e203aba81386c13dba62c3bca5719b86e4f32
Oleh Lavro <oleglavro6@gmail.com>
2021-10-31 01:10:54 +0300

    partical energy

--- Assets/Scripts/Flashing.cs
+++ Assets/Scripts/Flashing.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Flashing : MonoBehaviour
 {
+    private ParticleSystem _partical;
+    private void Start()
+    {
+        _partical = GetComponentInChildren<ParticleSystem>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            _partical.Play();
             collision.gameObject.GetComponent<PlayerController>().maxSpeed = 15;
-            Destroy(gameObject);
+            Invoke("Dead", 0.5f);
         }
+    }
+    void Dead()
+    {
+        Destroy(gameObject);
     }
 }
