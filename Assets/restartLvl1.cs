fd21ebef553f647026cf3b7392ca81af82808f18
disco w <this.disco.w@gmail.com>
2021-10-31 13:05:16 +0200

    Restart

---
+++ Assets/restartLvl1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartLvl1 : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            SceneManager.LoadScene(1);
+        }
+
+    }
+}
