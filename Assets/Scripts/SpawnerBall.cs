7aa213471de356fab71dda2460425bf0a23a4db4
Oleh Lavro <oleglavro6@gmail.com>
2021-10-31 13:01:29 +0200

    SpawnerBall

---
+++ Assets/Scripts/SpawnerBall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBall : MonoBehaviour
+{
+    [SerializeField] private float _timeToSpawn;
+    [SerializeField] private GameObject _energyBall;
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            Instantiate(_energyBall, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(_timeToSpawn);
+        }
+
+    }
+}
