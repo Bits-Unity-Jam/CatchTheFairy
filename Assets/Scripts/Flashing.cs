ff26c2343207c849fc62cd7b1cf156372a5cb565
Oleh Lavro <oleglavro6@gmail.com>
2021-10-31 10:23:22 +0200

    Sound energy

--- Assets/Scripts/Flashing.cs
+++ Assets/Scripts/Flashing.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Flashing : MonoBehaviour
 {
     private ParticleSystem _partical;
+    private AudioSource _aud;
     private void Start()
     {
+        _aud = GetComponent<AudioSource>();
         _partical = GetComponentInChildren<ParticleSystem>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            _aud.Play();
             _partical.Play();
             collision.gameObject.GetComponent<PlayerController>().maxSpeed = 15;
             Invoke("Dead", 0.5f);
