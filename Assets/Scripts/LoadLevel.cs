0c90450fb48836b76f253456c8f5b29143475e0d
disco w <this.disco.w@gmail.com>
2021-11-04 18:51:23 +0200

    Налаштував переходи

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -21,4 +21,8 @@ public class LoadLevel : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
