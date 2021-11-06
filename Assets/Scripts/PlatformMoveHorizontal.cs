ac8ebb989a2c4acd7bba9defa0412aa0493ab5c8
Oleh Lavro <oleglavro6@gmail.com>
2021-11-06 12:14:37 +0200

    -moving platform

--- Assets/Scripts/PlatformMoveHorizontal.cs
+++ Assets/Scripts/PlatformMoveHorizontal.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class PlatformMoveHorizontal : MonoBehaviour
 {
-    float dX;
-    float speed = 3f;
-    [SerializeField] private float moveTime;
-    bool moveRight = true;
-    float startMoveTime = Time.time;
+    //float dX;
+    //float speed = 3f;
+    //[SerializeField] private float moveTime;
+    //bool moveRight = true;
+    //float startMoveTime = Time.time;
 
 
-    void Update()
-    {
-        float deltaT = (Time.time - startMoveTime);
-        if (deltaT <= moveTime)
-        {
-            moveRight = false;
-        }
-        else if(deltaT > moveTime && deltaT <= moveTime * 2)
-        {
-            moveRight = true;
-        }
-        else
-        {
-            startMoveTime = Time.time;
-        }
+    //void Update()
+    //{
+    //    float deltaT = (Time.time - startMoveTime);
+    //    if (deltaT <= moveTime)
+    //    {
+    //        moveRight = false;
+    //    }
+    //    else if(deltaT > moveTime && deltaT <= moveTime * 2)
+    //    {
+    //        moveRight = true;
+    //    }
+    //    else
+    //    {
+    //        startMoveTime = Time.time;
+    //    }
 
-        if (moveRight)
-        {
-            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
-        }
-        else
-        {
-            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-        }
-    }
+    //    if (moveRight)
+    //    {
+    //        transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    //    }
+    //    else
+    //    {
+    //        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+    //    }
+    //}
 }
