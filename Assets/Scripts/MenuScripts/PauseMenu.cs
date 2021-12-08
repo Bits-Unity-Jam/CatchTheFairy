ac38b48e24f6dfafc23e6cfa10a653181da5876d
disco w <this.disco.w@gmail.com>
2021-12-08 23:17:20 +0200

    Музыка на уровнях, подправлен коллайдер, подправлены немного текстуры на 6 уровне.

--- Assets/Scripts/MenuScripts/PauseMenu.cs
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -17,17 +17,17 @@ public class PauseMenu : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    if (GameIsPaused)
+        //    {
+        //        Resume();
+        //    }
+        //    else
+        //    {
+        //        Pause();
+        //    }
+        //}
     }
 
     //public void GamePause()
